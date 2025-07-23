ae559dce719ae32208042f1cf3c18737fb40c03c
EduardoArce03 <eduardomaldonado2003ortiz@gmail.com>
2025-07-22 22:45:42 -0500

    Exa

---
+++ src/controllers/MaquinaController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import models.Maquina;
+
+public class MaquinaController {
+
+    public Stack<Maquina> filtrarPorSubred(List<Maquina> maquina , int umbral){
+
+        
+    }
+    public Set<Maquina> ordenarPorSubred(Stack<Maquina> pila){
+        
+    }
+    public Map<Integer,Queue<Maquina>> agruparPorRiesgo(List<Maquina> maquinas){
+        
+    }
+    public Stack<Maquina> explorarGrupo(Map<Integer,Queue<Maquina>> mapa){
+
+    }
+
+}
