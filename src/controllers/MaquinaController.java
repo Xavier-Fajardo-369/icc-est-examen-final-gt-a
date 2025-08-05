13533422fc51013af6e42d72f2d244d1fbcc9a7d
Xavier Fajardo <fajardoxavier61@gmail.com>
2025-08-05 15:58:04 -0500

    R

--- src/controllers/MaquinaController.java
+++ src/controllers/MaquinaController.java
@@ -4,7 +4,6 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -20,17 +19,18 @@ public class MaquinaController {
     public Stack<Maquina> filtrarPorSubred(List<Maquina> maquinas, int umbral) {
         Stack<Maquina> pila = new Stack<>();
         for (Maquina m : maquinas) {
-            if (m.getSubred() < umbral) {  // corregido menor que umbral
+            if (m.getSubred() > umbral) {  // filtro para subred > umbral
                 pila.push(m);
             }
         }
         return pila;
     }
+
     public Set<Maquina> ordenarPorSubred(Stack<Maquina> maquinas) {
         Comparator<Maquina> comp = Comparator
             .comparingInt(Maquina::getSubred)
             .thenComparing(Maquina::getNombre);
-    
+
         TreeSet<Maquina> ordenado = new TreeSet<>(comp);
         ordenado.addAll(maquinas);
         return ordenado;
@@ -51,7 +51,6 @@ public class MaquinaController {
         int maxRiesgo = Integer.MIN_VALUE;
         Queue<Maquina> grupoSeleccionado = null;
 
-       
         for (Map.Entry<Integer, Queue<Maquina>> entry : mapa.entrySet()) {
             int riesgo = entry.getKey();
             int cantidad = entry.getValue().size();
@@ -65,9 +64,7 @@ public class MaquinaController {
 
         Stack<Maquina> pila = new Stack<>();
         if (grupoSeleccionado != null) {
-            
             List<Maquina> lista = new ArrayList<>(grupoSeleccionado);
-            
             for (int i = lista.size() - 1; i >= 0; i--) {
                 pila.push(lista.get(i));
             }
